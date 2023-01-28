bc9206050c3d96c0ff859e51ea2a34e25a63f510
Иван Кушнир <kushnir82@mail.ru>
2023-01-28 18:15:53 +0300

    дз 1 семинара

--- home_lesson_1/task_3.py
+++ home_lesson_1/task_3.py
@@ -13,13 +13,25 @@ if len(ticket) != 6:
 else:
     sum_1 = 0
     sum_2 = 0
-    num_1 = [int(i) for i in ticket[0: 3]]
-    for i in num_1:
-        sum_1 = i + sum_1
-    num_2 = [int(i) for i in ticket[-3:]]
-    for i in num_2:
-        sum_2 = i + sum_2
+    for i in ticket[:3]:
+        i = int(i)
+        sum_1 += i 
+    for i in ticket[-3:]:
+        i = int(i)
+        sum_2 += i
     if sum_1 == sum_2:
         print("это счастливый билет!!!")
     else:
-        print("это НЕ счатливый билет :( ")
+        print("это НЕ счатливый билет :( ")    
+
+    # num_1 = [int(i) for i in ticket[0: 3]]
+    # for i in num_1:
+    #     sum_1 = i + sum_1
+    # num_2 = [int(i) for i in ticket[-3:]]
+    # for i in num_2:
+    #     sum_2 = i + sum_2
+    # if sum_1 == sum_2:
+    #     print("это счастливый билет!!!")
+    # else:
+    #     print("это НЕ счатливый билет :( ")
+    
