2d18f8b266603c3d897a8a4448165b50ac8c5131
Иван Кушнир <kushnir82@mail.ru>
2023-02-06 17:56:44 +0300

    ДЗ 3 семинара

--- list_call.py
+++
@@ -1,7 +0,0 @@
-
-# Варианты набора списков 
-b = list(range(1, 8))
-print(b)
-
-a = []
-[a.append(i) for i in range(1, int(input()) + 1)]
--- random_use.py
+++
@@ -1,7 +0,0 @@
-
-import random
-
-# Набор массива длиною введенной пользователем случайными целыми числами.
-a = []
-[a.append(random.randint(1, 10)) for i in range(1, int(input()) + 1)]
-
